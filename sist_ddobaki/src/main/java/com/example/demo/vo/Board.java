45fb9a01d5d833f91f43317e7c46312d3c2c9d57
juhee ko <lala8524@gmail.com>
2022-06-29 15:32:37 +0900

    Merge branch 'master' of https://github.com/pearlrabbit/sist_ddobaki.git

--- sist_ddobaki/src/main/java/com/example/demo/vo/Board.java
+++ sist_ddobaki/src/main/java/com/example/demo/vo/Board.java
@@ -14,6 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 
+
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
