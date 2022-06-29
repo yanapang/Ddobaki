c305d57c6f83a119aa546f5b42d72af32f99f255
iiolo <alohalo98@naver.com>
2022-06-29 15:45:03 +0900

    place entity 추가

---
+++ sist_ddobaki/src/main/java/com/example/demo/vo/Place.java
@@ -0,0 +1,96 @@
+package com.example.demo.vo;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="place")
+public class Place {
+	@Id
+	private int place_num;
+	
+	@Column(nullable = false)
+	private int	place_region_num;
+	
+	@Column(nullable = false)
+	private int	place_type_num;	
+	
+	@Column(nullable = false)
+	private String place_name;
+	
+	@Column(nullable = true)
+	private String place_explain;
+	
+	@Column(nullable = false)
+	private String place_addr;
+	
+	@Column(nullable = true)
+	private String place_site;
+	
+	@Column(nullable = false, columnDefinition = "number default 0")
+	private int	place_weight;
+	
+	@Column(nullable = true)
+	private String place_tel;
+	
+	@Column(nullable = false, columnDefinition = "number default 0")
+	private int	place_park;	
+	
+	@Column(nullable = false, columnDefinition = "number default 0")
+	private int	place_spa;
+	
+	@Column(nullable = false, columnDefinition = "number default 0")
+	private int	place_meal;	
+	
+	@Column(nullable = true)
+	private int	place_park_price;
+	
+	@Column(nullable = true)
+	private int	place_spa_price;
+	
+	@Column(nullable = true)
+	private int	place_meal_price;
+	
+	@Column(nullable = false, columnDefinition = "varchar2(500) default 'default.jpg'")
+	private String place_thumbnail;	
+	
+	@Column(nullable = true)
+	private String place_detail;	
+	
+	@Column(nullable = false, columnDefinition = "number default 0")
+	private int	place_hit;
+	
+	@Column(nullable = false)
+	private String place_lat;
+	
+	@Column(nullable = false)
+	private String place_lng;	
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<Board> board;
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<Dibs> dibs;
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE )
+	private List<Reservation> reservation;
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<Room> room;
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<PlaceImage> placeImage;
+	
+	@OneToMany(mappedBy="place", fetch=FetchType.LAZY, cascade = CascadeType.REMOVE)
+	private List<Plan> plan;
+}
