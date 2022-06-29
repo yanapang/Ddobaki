02616e01dd64a8be75fffee71424f2a067e1d49a
kimdajeong <kimdajeong@localhost>
2022-06-29 16:21:41 +0900

    PlanController 추가 [김다정]

---
+++ sist_ddobaki/src/main/java/com/example/demo/controller/PlanController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlanController {
+	@GetMapping("/insertPlan")
+	public int insert() {
+		return 1;
+	}
+}
