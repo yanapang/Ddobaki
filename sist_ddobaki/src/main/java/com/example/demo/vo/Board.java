37c11d83c7aae7f294b9caa011f64191447a38e0
juhee ko <lala8524@gmail.com>
2022-06-29 15:29:36 +0900

    Board 엔티티

--- sist_ddobaki/src/main/java/com/example/demo/vo/Board.java
+++ sist_ddobaki/src/main/java/com/example/demo/vo/Board.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
