febcd74b1e24edf540fc33729e3dcfb8270bcdbc
yana711 <yana711@users.noreply.github.com>
2022-06-29 16:26:12 +0900

    plan controller commit test

---
+++ sist_ddobaki/src/main/java/com/example/demo/vo/PlanController.java
@@ -0,0 +1,13 @@
+package com.example.demo.vo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PlanController {
+
+	@GetMapping("insertPlan")
+	public String insert() {
+		return "OK";
+	}
+}
