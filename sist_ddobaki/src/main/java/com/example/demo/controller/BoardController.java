3f7c7a4cccf349dfe0a2cda88cfa655af67c50ad
juhee ko <lala8524@gmail.com>
2022-07-05 15:56:06 +0900

    BoardController 추가-상세보기/페이지[고주희]

--- sist_ddobaki/src/main/java/com/example/demo/controller/BoardController.java
+++ sist_ddobaki/src/main/java/com/example/demo/controller/BoardController.java
@@ -1,28 +1,68 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.service.BoardService;
 import com.example.demo.vo.Board;
 
 import lombok.Setter;
 
-@RestController
+@Controller
 @Setter
 public class BoardController {
+	
 	@Autowired
 	private BoardService bs;
-
+	
+	/*
+	@GetMapping("/listlist")
+	public String list(Model model) {
+		List<Board> board = bs.findAll();
+		model.addAttribute("board",board);
+		return "redirect:/listBoard";
+	}
+	*/
+	
+	/*
 	@GetMapping("/listBoard")
-	public List<Board> listBoard() {
+	public List<Board> findAll(){	
 		return bs.findAll();
 	}
+	*/
+	
+	@GetMapping("/listBoard")
+	public void listBoard(Model model) {
+		model.addAttribute("list", bs.findAll());
+	}
+	
+	//카테고리 하나 눌렀을때 그 페이지로
+	@GetMapping("/category/{board_num}")
+	public ModelAndView goCategory(@PathVariable int board_num,Model model) {
+		ModelAndView mav = new ModelAndView("category");
+		model.addAttribute("boardCategory", bs.goCategory(board_num));
+		return mav;
+	}
+	
+	//상세보기 눌렀을때 board_num도 함께 가도록
+	@GetMapping("/detailPost/{board_num}/{post_num}")
+	public ModelAndView detailPost(@PathVariable int board_num,@PathVariable int post_num, Model model) {
+		ModelAndView mav = new ModelAndView("detailPost");
+		model.addAttribute("detail",bs.detailPost(board_num, post_num));
+		return mav;
+	}
+	
 	
 //	@GetMapping("/insertBoard")
 //	public void insert(Model model) {
