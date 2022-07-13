ad9111b84cc2d2622db2f846097f54cab01cdfd7
juhee ko <lala8524@gmail.com>
2022-07-13 14:16:45 +0900

    수정[진주]

--- sist_ddobaki/src/main/java/com/example/demo/SistDdobakiApplication.java
+++ sist_ddobaki/src/main/java/com/example/demo/SistDdobakiApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-
 @SpringBootApplication
 public class SistDdobakiApplication {
 
