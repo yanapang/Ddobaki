b589b4d218770e84b758541d62cc0c8ca40657d2
yanapang <yanahwang@gmail.com>
2022-07-20 16:38:53 +0900

    플랜 입력 페이지 css 수정 [7/20 예은]

--- sist_ddobaki/src/main/resources/static/js/plan.js
+++ sist_ddobaki/src/main/resources/static/js/plan.js
@@ -22,7 +22,7 @@ $(function() {
 	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
 		mapOption = {
 			center: new kakao.maps.LatLng(37.556317, 126.922713), // 지도의 중심좌표
-			level: 3 // 지도의 확대 레벨
+			level: 6 // 지도의 확대 레벨
 		};
 
 	//지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
